4a4b7f1cafa4dce95c21cdc9283fbc54220a7a4b
m-sarabi <m.sarabi.jkd@gmail.com>
2023-04-23 14:17:17 +0430

    More messages

--- pypassgen.py
+++ pypassgen.py
@@ -1,6 +1,7 @@
 import generator
 import PySimpleGUI as sg
 import pyperclip
+
 # prompt the user for the length of the password
 length = int(sg.popup_get_text('Enter password length', 'Password Length'))
 
@@ -18,3 +19,6 @@ pyperclip.copy(password)
 
 # show the generated password to the user in another popup
 sg.popup('Your password is:' + password)
+
+sg.popup_no_buttons("Generated password is copied to clipboard", title='PyPassGen', no_titlebar=True, auto_close=True,
+                    modal=False)
