8750d9bee96b1238e9e9105bba84694d46887bbd
m-sarabi <m.sarabi.jkd@gmail.com>
2023-04-23 14:07:51 +0430

    Initial GUI for testing

--- pypassgen.py
+++ pypassgen.py
@@ -1,10 +1,20 @@
 import generator
+import PySimpleGUI as sg
+import pyperclip
+# prompt the user for the length of the password
+length = int(sg.popup_get_text('Enter password length', 'Password Length'))
 
-# prompt the user for input
-length = int(input("Enter the length of the password: "))
-num_special_chars = int(input("Enter the number of special characters: "))
+# prompt the user for the number of special characters
+num_special_chars = int(sg.popup_get_text('Enter number of special characters', 'Special Characters'))
+
+# prompt the user for the excluded characters
+excl_chars = sg.popup_get_text('Enter the characters to be excluded', 'Excluded Characters')
 
 # generate and print the password
-password = generator.generate_password(length, num_special_chars, '')
-print("Your password is:")
-print(password)
+password = generator.generate_password(length, num_special_chars, excl_chars)
+
+# copy the password to the system clipboard
+pyperclip.copy(password)
+
+# show the generated password to the user in another popup
+sg.popup('Your password is:' + password)
